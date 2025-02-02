b13f413b30357250d19b8eb4d6b9bffc804ef048
송정호 <50672132+dynamite885@users.noreply.github.com>
2025-02-02 18:55:37 +0900

    [Bronze I] Title: Headshot, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- Python/백준/Bronze/3546. Headshot/Headshot.py
+++ Python/백준/Bronze/3546. Headshot/Headshot.py
@@ -1,8 +1,7 @@
 *s,=map(int,input())
-if max(s)<1:exit(print("EQUAL"))
 t=s[-1]
 a=b=0
 for i in s:
  if t<1:a+=i;b+=1-i
  t=i
-print("RESOQHTUOAAOTLTE"[1+(a<b)-(a>b)::3])
+print("RESOQHTUOAAOTLTE"[1+(0<a<b)-(a>b)::3])
