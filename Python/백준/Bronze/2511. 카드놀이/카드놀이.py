e688a923116b62ad026071b06fc1d4189478c01d
송정호 <50672132+dynamite885@users.noreply.github.com>
2025-10-02 23:13:14 +0900

    [Bronze II] Title: 카드놀이, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- Python/백준/Bronze/2511. 카드놀이/카드놀이.py
+++ Python/백준/Bronze/2511. 카드놀이/카드놀이.py
@@ -1,3 +1,4 @@
+k=[1,3,0]
 a=b=c=0
-for i,j in zip(input()[::2],input()[::2]):a+=[1,3,0][t:=(j<i)-(i<j)];b+=[1,3,0][-t];c=t or c
+for i,j in zip(input()[::2],input()[::2]):a+=k[t:=(j<i)-(i<j)];b+=k[-t];c=t or c
 print(a,b,'DAB'[(b<a)-(a<b)or c])
