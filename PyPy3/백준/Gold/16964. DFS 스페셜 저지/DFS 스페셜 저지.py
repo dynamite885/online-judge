a860cb4375c0f2e343c312053a8be554475c07ba
송정호 <50672132+dynamite885@users.noreply.github.com>
2025-10-14 21:02:55 +0900

    [Gold III] Title: DFS 스페셜 저지, Time: 540 ms, Memory: 133396 KB -BaekjoonHub

---
+++ PyPy3/백준/Gold/16964. DFS 스페셜 저지/DFS 스페셜 저지.py
@@ -0,0 +1,26 @@
+input = open(0).readline
+n = int(input())
+s = set()
+for _ in range(n - 1):
+    u, v = map(int, input().split())
+    s.add((min(u, v), max(u, v)))
+order = [*map(int, input().split())]
+if order[0] != 1:
+    print(0)
+    exit()
+skip = [1] * n
+p, q = 0, 1
+while s:
+    if p < 0 or n <= q:
+        print(0)
+        exit()
+    start, end = order[p], order[q]
+    node = (min(start, end), max(start, end))
+    if node in s:
+        s -= {node}
+        p = q
+        q += 1
+    else:
+        skip[p + 1] += skip[p]
+        p -= skip[p]
+print(1)
