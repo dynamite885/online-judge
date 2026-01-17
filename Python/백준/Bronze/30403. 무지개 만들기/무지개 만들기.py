0fdff4552bcbecbbfa87629bbaff35667e7970ed
송정호 <50672132+dynamite885@users.noreply.github.com>
2026-01-17 23:37:39 +0900

    [Bronze II] Title: 무지개 만들기, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

---
+++ Python/백준/Bronze/30403. 무지개 만들기/무지개 만들기.py
@@ -0,0 +1,8 @@
+input()
+s=input()
+lower={*"roygbiv"}
+upper={*"ROYGBIV"}
+if lower<={*s} and upper<={*s}:exit(print("YeS"))
+if lower<={*s}:exit(print("yes"))
+if upper<={*s}:exit(print("YES"))
+print("NO!")
